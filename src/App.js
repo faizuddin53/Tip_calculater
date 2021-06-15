b2b21deae2d9926207ba25e3eb4436c77b4933e0
faizuddin <faizuddinansari40@gmail.com>
2021-06-15 20:26:18 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import Header from './componants/Header'
+import TipCalculator from './componants/TipCalculator'
+import Footer from './componants/Footer'
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <TipCalculator />
+      <Footer />
+
+    </div>
+
+  );
+}
+
+export default App;
